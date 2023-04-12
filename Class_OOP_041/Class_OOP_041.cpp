a12e19b1503c1b66df89d97c4c16623ce5f20b61
abrarimamsatria09 <092004abrar@gmail.com>
2023-04-12 16:04:07 +0700

    memasukkan data nilai

--- Class_OOP_041/Class_OOP_041.cpp
+++ Class_OOP_041/Class_OOP_041.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
-
 class mahasiswa {
 
 private:
 	int nim;
 	string nama;
+public:
+	void inputdata() {
+		cout << "masukkan NIM : ";
+		cin >> nim;
+		cout << "masukkan Nama : ";
+		cin >> nama;
+	}
+};
